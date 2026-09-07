279cb4e6579dc2308d12da8a591dc11ed00362f3
Your Name <hlomello765@gmail.com>
2026-09-07 17:28:15 +0530

    feat : updated vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GEMINI_API_KEY: string;
+  // 100% Client-side application with zero external APIs
 }
 
 interface ImportMeta {
